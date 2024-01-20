b03d84d7167036344f7ed63a022c501d1f802a37
Avadesh Chaubey <avadesh.chaubey@genmills.com>
2024-01-20 06:43:15 -0600

    final

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World enjoy my code!');
+  res.send('Hello World, This code is being deployed throgh cicd pipline');
 });
 
 app.listen(PORT, HOST, () => {
