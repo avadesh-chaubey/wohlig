09d878891f1902b3fa3c561c29bbdafbbfc42246
Avadesh Chaubey <avadesh.chaubey@genmills.com>
2024-01-20 23:21:40 -0600

    new deploy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Nginx Page downloading server from cloud');
+  res.send('Nginx Page from asia');
 });
 
 app.listen(PORT, HOST, () => {
