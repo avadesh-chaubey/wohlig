09e7d2f890f7713fa430517d0e1a79da98fbf3b5
Avadesh Chaubey <avadesh.chaubey@genmills.com>
2024-05-24 16:12:02 +0530

    code changed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Nginx Page from terraform From v5');
+  res.send('Nginx Page from terraform From trivy');
 });
 
 app.listen(PORT, HOST, () => {
